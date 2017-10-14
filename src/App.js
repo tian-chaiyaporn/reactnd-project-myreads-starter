d64c442b79e7dbc44e135482c3a068f7fb2ee925
tian <tian_chaiya12@hotmail.com>
2017-10-14 21:49:02 +0700

    utillize BooksAPI to make changes to shelves for books persistent across sessions

--- src/App.js
+++ src/App.js
@@ -7,52 +7,29 @@ import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
-    bookList: [
-      {
-        title: 'To Kill a Mockingbird',
-        coverUrl: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
-        author: 'Harper Lee',
-        shelf: 'currentlyReading'
-      },
-      {
-        title: '1776',
-        coverUrl: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
-        author: 'David McCullough',
-        shelf: 'wantToRead'
-      },
-      {
-        title: "Ender's Game",
-        coverUrl: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
-        author: 'Orson Scott Card',
-        shelf: 'currentlyReading'
-      },
-      {
-        title: "Harry Potter and the Sorcerer's Stone",
-        coverUrl: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
-        author: 'J.K. Rowling',
-        shelf: 'wantToRead'
-      },
-      {
-        title: 'The Hobbit',
-        coverUrl: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
-        author: 'J.R.R. Tolkien',
-        shelf: 'read'
-      },
-      {
-        title: "Oh, the Places You'll Go!",
-        coverUrl: 'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
-        author: 'Seuss',
-        shelf: 'read'
-      },
-      {
-        title: 'The Adventures of Tom Sawyer',
-        coverUrl: 'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api',
-        author: 'Mark Twain',
-        shelf: 'read'
-      }
-    ],
+    bookList: [],
     query: '',
     searchBooks: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(payload => {
+        return payload.map(book => {
+          return {
+            id: book.id,
+            title: book.title,
+            coverUrl: book.imageLinks ? book.imageLinks.thumbnail : 'http://via.placeholder.com/128x193?text=No%20Cover',
+            author: book.authors ? book.authors.reduce((a, b) => a + ', ' + b) : 'unknown',
+            shelf: book.shelf
+          }
+        })
+      })
+      .then(payload => {
+        this.setState({
+          bookList: payload
+        })
+      })
   }
 
   updateQuery = (query) => {
@@ -68,6 +45,7 @@ class BooksApp extends React.Component {
         } else {
           return res.map(bookResult => {
             return {
+              id: bookResult.id,
               title: bookResult.title,
               coverUrl: bookResult.imageLinks ? bookResult.imageLinks.thumbnail : 'http://via.placeholder.com/128x193?text=No%20Cover',
               author: bookResult.authors ? bookResult.authors.reduce((a, b) => a + ', ' + b) : 'unknown',
@@ -87,23 +65,32 @@ class BooksApp extends React.Component {
   }
 
   changeShelf = (book, shelfName) => {
-    this.setState((state) => ({
-      bookList: state.bookList.map(b => {
-        if (b.title === book.title) {
-          b.shelf = shelfName
-        }
-        return b;
+    BooksAPI
+      .update(book, shelfName)
+      .then(() => {
+        this.setState((state) => ({
+          bookList: state.bookList.map(b => {
+            if (b.id === book.id) {
+              b.shelf = shelfName
+            }
+            return b;
+          })
+        }))
       })
-    }))
   }
 
   addToShelf = (searchBook, shelfName) => {
     const currentBookList = this.state.bookList;
-    if (currentBookList.filter(b => b.title === searchBook.title).length === 0) {
-      searchBook.shelf = shelfName
-      this.setState((state) => ({
-        bookList: state.bookList.concat(searchBook)
-      }))
+    if (currentBookList.filter(b => b.id === searchBook.id).length === 0) {
+      BooksAPI
+        .get(searchBook.id)
+        .then((receivedBook) => {
+          this.changeShelf(receivedBook, shelfName);
+          searchBook.shelf = shelfName;
+          this.setState((state) => ({
+            bookList: state.bookList.concat(searchBook)
+          }))
+        })
     } else {
       this.changeShelf(searchBook, shelfName)
     }
